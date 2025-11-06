79c2cb275d74116d8b7a92c30b9b88fb29b41e53
koot0303 <sashou.t0303@gmail.com>
2025-11-06 10:03:54 +0900

    Update urls.py 牌譜用のエンドポイント追加

--- mahjong_realtime_simulator/app/urls.py
+++ mahjong_realtime_simulator/app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import mahjong_render, main
+from .views import mahjong_render, main, tiles_save
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('mahjong_render/', mahjong_render, name='mahjong_render'),
-    path('main/', main, name='main'),
+    path('mahjong_render/', mahjong_render, name='mahjong_render'), # アクセス用URL
+    path('main/', main, name='main'), # 計算実行のエンドポイント
+    path('tiles_save/', tiles_save, name='tiles_save'), # 牌譜記録のエンドポイント
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
